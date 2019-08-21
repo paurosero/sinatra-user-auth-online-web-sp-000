280dd7d56309e91f36cccd4ce8ce609395c0354c
Paul Rosero <paul_rosero@yahoo.com>
2019-08-21 22:19:32 +0000

    Automatically backed up by Learn

--- db/migrate/20190820010825_create_users.rb
+++ db/migrate/20190820010825_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |u|
       u.string :name 
-      u.sring :email 
+      u.string :email 
       u.string :password
     end
   end
